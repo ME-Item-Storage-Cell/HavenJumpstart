5684d0cf6d447ab303f681a9b05b46cdcc8d02eb
256K Item Storage Cell <homlesskiddos@gmail.com>
2026-09-01 15:01:09 +1000

    Organising
    Rename some files to be more descriptive.  Started on the limited inputs system

--- PlayerOne/MovementP1.gd
+++ PlayerOne/MovementP1.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var inputCount = 0
+
+@onready var inputCounter = $InputCounterP1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,13 +16,16 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jumpone") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		inputCount += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("leftone", "rightone")
 	if direction:
 		velocity.x = direction * SPEED
+		inputCount += 1
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		inputCount += 1
 
 	move_and_slide()
