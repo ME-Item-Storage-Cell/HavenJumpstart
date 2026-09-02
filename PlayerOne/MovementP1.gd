3f069391098b2b21a70d4f98201f33c909c7b869
256K Item Storage Cell <homlesskiddos@gmail.com>
2026-09-03 08:35:12 +1000

    Physics overhall
    Slightly oiled
    Still need to fix stacking mechanics

--- PlayerOne/MovementP1.gd
+++ PlayerOne/MovementP1.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+const DRAG = 0.05
+const ACCEL = 0.10
+const DECCEL = 1000
 
 var inputCount = 10
 var inRange = false
@@ -19,33 +21,39 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jumpone") and is_on_floor() and inputCount > 0:
-		velocity.y = JUMP_VELOCITY
-		inputCount -= 1
-		
-		
-	if (Input.is_action_just_pressed("leftone") or Input.is_action_just_pressed("rightone")) and inputCount > 0:
-		inputCount -= 1
+		velocity.y += JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("leftone", "rightone")
 	if direction and inputCount > 0:
-		velocity.x = direction * SPEED
+		velocity.x += direction * SPEED * ACCEL
+		velocity.x = clamp(velocity.x, -(SPEED), SPEED)
 
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	if Input.is_action_just_pressed("jumpstartone") and inRange and playerTwo.inputcount == 0:
-		playerTwo.inputCount = 10
-		print("help!")
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, DECCEL * delta)
+		else:
+			velocity.x = move_toward(velocity.x, 0, DECCEL * delta * DRAG)
 
 	move_and_slide()
+	
+	if is_on_ceiling() and direction != 0 and inputCount > 0:
+		for i in get_slide_collision_count():
+			var collision = get_slide_collision(i)
+			var collider = collision.get_collider()
+			
+			if collider == playerTwo:
+				position.x += direction * SPEED * delta
+				
+	if (Input.is_action_just_released("leftone") or Input.is_action_just_released("rightone") or Input.is_action_just_released("jumpone")) and inputCount > 0:
+		inputCount -= 1
+	
+	if Input.is_action_just_pressed("jumpstartone") and inRange and playerTwo.inputCount == 0 and inputCount != 0:
+		playerTwo.inputCount = 10
 
 
 func _on_jumpstart_area_p_2_body_entered(_body: Node2D) -> void:
 	inRange = true
-	print("hi 2")
 
 func _on_jumpstart_area_p_2_body_exited(_body: Node2D) -> void:
 	inRange = false
-	print("bye 2")
--- PlayerTwo/MovementP2.gd
+++ PlayerTwo/MovementP2.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+const DRAG = 0.05
+const ACCEL = 0.10
+const DECCEL = 1000
 
 var inputCount = 10
-var playerInRange: CharacterBody2D = null
 var inRange = false
 @onready var inputCounter = $InputCounterP2
 @onready var playerOne = get_node("../Player 1")
@@ -19,25 +21,38 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jumptwo") and is_on_floor() and inputCount > 0:
 		velocity.y = JUMP_VELOCITY
-		inputCount -= 1
-		
-	if (Input.is_action_just_pressed("lefttwo") or Input.is_action_just_pressed("righttwo")) and inputCount > 0:
-		inputCount -= 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("lefttwo", "righttwo")
 	if direction and inputCount > 0:
-		velocity.x = direction * SPEED
+		velocity.x += direction * SPEED * ACCEL
+		velocity.x = clamp(velocity.x, -(SPEED), SPEED)
+		print(velocity.x)
 
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	if Input.is_action_just_pressed("jumpstarttwo") and inRange and playerOne.inputCount == 0:
-		playerOne.inputCount = 10
-		print("help!")		
+		print("gay")
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, DECCEL * delta)
+		else:
+			velocity.x = move_toward(velocity.x, 0, DECCEL * delta * DRAG)
 
 	move_and_slide()
+	
+	if is_on_ceiling() and direction != 0 and inputCount > 0:
+		for i in get_slide_collision_count():
+			var collision = get_slide_collision(i)
+			var collider = collision.get_collider()
+			
+			if collider == playerOne:
+				position.x += direction * SPEED * delta
+				
+	if (Input.is_action_just_released("lefttwo") or Input.is_action_just_released("righttwo") or Input.is_action_just_pressed("jumptwo")) and inputCount > 0:
+		inputCount -= 1
+
+	if Input.is_action_just_pressed("jumpstarttwo") and inRange and playerOne.inputCount == 0 and inputCount > 0:
+		playerOne.inputCount = 10
+		print("help!")		
 
 
 func _on_jumpstart_area_p_1_body_entered(_body: Node2D) -> void:
