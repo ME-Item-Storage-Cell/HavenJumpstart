dfb171a93d82d7cd9c6f8c3c760d94d7f8245e51
256K Item Storage Cell <homlesskiddos@gmail.com>
2026-09-02 11:37:16 +1000

    Kickstart + Input limit
    players cannot move when they run out of moves. Other player can "kickstart" their friend and reset their moves.
    
    Next update will probably be physics

--- PlayerOne/MovementP1.gd
+++ PlayerOne/MovementP1.gd
@@ -4,28 +4,48 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-var inputCount = 0
-
+var inputCount = 10
+var inRange = false
 @onready var inputCounter = $InputCounterP1
+@onready var playerTwo = get_node("../Player 2")
 
 func _physics_process(delta: float) -> void:
+	
+	inputCounter.text = str(inputCount)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jumpone") and is_on_floor():
+	if Input.is_action_just_pressed("jumpone") and is_on_floor() and inputCount > 0:
 		velocity.y = JUMP_VELOCITY
-		inputCount += 1
+		inputCount -= 1
+		
+		
+	if (Input.is_action_just_pressed("leftone") or Input.is_action_just_pressed("rightone")) and inputCount > 0:
+		inputCount -= 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("leftone", "rightone")
-	if direction:
+	if direction and inputCount > 0:
 		velocity.x = direction * SPEED
-		inputCount += 1
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		inputCount += 1
+		
+	if Input.is_action_just_pressed("jumpstartone") and inRange and playerTwo.inputcount == 0:
+		playerTwo.inputCount = 10
+		print("help!")
 
 	move_and_slide()
+
+
+func _on_jumpstart_area_p_2_body_entered(_body: Node2D) -> void:
+	inRange = true
+	print("hi 2")
+
+func _on_jumpstart_area_p_2_body_exited(_body: Node2D) -> void:
+	inRange = false
+	print("bye 2")
--- PlayerTwo/MovementP2.gd
+++ PlayerTwo/MovementP2.gd
@@ -4,22 +4,46 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var inputCount = 10
+var playerInRange: CharacterBody2D = null
+var inRange = false
+@onready var inputCounter = $InputCounterP2
+@onready var playerOne = get_node("../Player 1")
 
 func _physics_process(delta: float) -> void:
+	inputCounter.text = str(inputCount)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jumptwo") and is_on_floor():
+	if Input.is_action_just_pressed("jumptwo") and is_on_floor() and inputCount > 0:
 		velocity.y = JUMP_VELOCITY
+		inputCount -= 1
+		
+	if (Input.is_action_just_pressed("lefttwo") or Input.is_action_just_pressed("righttwo")) and inputCount > 0:
+		inputCount -= 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("lefttwo", "righttwo")
-	if direction:
+	if direction and inputCount > 0:
 		velocity.x = direction * SPEED
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if Input.is_action_just_pressed("jumpstarttwo") and inRange and playerOne.inputCount == 0:
+		playerOne.inputCount = 10
+		print("help!")		
+
 	move_and_slide()
+
+
+func _on_jumpstart_area_p_1_body_entered(_body: Node2D) -> void:
+	inRange = true
+	print("hi 1")
+
+func _on_jumpstart_area_p_1_body_exited(_body: Node2D) -> void:
+	inRange = false
+	print("bye 1")
