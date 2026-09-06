ddc2bfc510c6c24b8a0364f3560e026306ce1d29
256K Item Storage Cell <homlesskiddos@gmail.com>
2026-09-06 16:47:02 +1000

    basic main menu
    lets you start (into the testing scene as level select not yet implemented) or quite the game

---
+++ MainMenu/main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/Testing/main.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
