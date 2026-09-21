af81f5fbca7759c0e7061ed2a01d33cdc2f6091e
256K Item Storage Cell <homlesskiddos@gmail.com>
2026-09-21 22:08:51 +1000

    Game submission!
    Door and button textures, and 5 levels in total.
    
    The cake?

---
+++ Levels/5.gd
@@ -0,0 +1,29 @@
+extends Node
+
+@export var spawnPointP1 = Node2D
+@export var player1 = Node2D
+
+@export var spawnPointP2 = Node2D
+@export var player2 = Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player1.global_position = spawnPointP1.global_position
+	player2.global_position = spawnPointP2.global_position
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+
+
+func _on_goal_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Players"):
+		get_tree().call_deferred("change_scene_to_file", "res://Levels/credits.tscn")
+
+
+func _on_relief_body_entered(body: Node2D) -> void:
+	player1.inputCount = 10
+	player2.inputCount = 10
--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	pass
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("jumpone") or Input.is_action_just_pressed("jumptwo"):
+	if Input.is_action_just_pressed("jumpone") or Input.is_action_just_pressed("jumptwo") or Input.is_action_just_pressed("jumpthree"):
 		get_tree().call_deferred("change_scene_to_file", "res://Levels/1.tscn")
 
 
